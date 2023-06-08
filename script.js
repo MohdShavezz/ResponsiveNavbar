e335bef16834b257fd6e35d11325c46704b5c325
MohdShvezz <mohdpctebtech19cse@gmail.com>
2023-06-08 12:43:30 +0530

    'navbar'

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function hideShow() {
+  const icon = document.getElementById('hamburger')
+  icon.classList.toggle('fa-times')
+  icon.classList.toggle('fa-bars')
+  document.getElementById('navId').classList.toggle('show-nav')
+}
+
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.querySelector("header").style.padding =  "20px";
+    document.querySelector(".sailor").style.fontSize =  "24px";
+  } else {
+    document.querySelector("header").style.padding = "28px";
+    document.querySelector(".sailor").style.fontSize ="28px";
+  }
+}
